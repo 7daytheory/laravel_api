d94b5010ae1e0723c8e677c70356672711dff184
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-02-08 16:41:30 -0500

    Fixed api Route resource issue, updated items Controller

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
 
 class ItemsController extends Controller
 {
@@ -13,7 +14,10 @@ class ItemsController extends Controller
      */
     public function index()
     {
-        //
+        //Retrieve items
+        $items = Item::get();
+
+        return $items;
     }
 
     /**
